65d4ba47666063aa2cf1acc7e8b830f165a81084
Thomas <thomas@lepamplemousse.fr>
2018-05-26 07:38:08 +0200

    Add default twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,16 +5,12 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
     /**
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
-    {
-        return $this->render('@App/default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+    public function indexAction(Request $request) {
+        return $this->render('@App/default/index.html.twig');
     }
 }
